2131e81695b87d20614b41ed829aacba8b2ee459
Mathijs van de Nes <mvdnes@mvdnes.nl>
2014-01-26 19:22:25 +0100

    Implemented part of MBC1

--- src/mmu.rs
+++ src/mmu.rs
@@ -2,9 +2,6 @@ use serial::Serial;
 use timer::Timer;
 use std::io::File;
 
-//mod serial;
-//mod timer;
-
 static WRAM_SIZE: uint = 0x2000;
 //static ZRAM_SIZE: uint = 0x7F;
 static ZRAM_SIZE: uint = 0x100;
@@ -18,8 +15,18 @@ pub struct MMU {
 	intf: u8,
 	priv serial: Serial,
 	priv timer: Timer,
+	priv mbc: MBC,
+	priv rombank: u16,
+	priv rambank: u16,
+	priv mbcmode: bool,
+	priv ramon: bool,
 }
 
+enum MBC {
+	Direct,
+	MBC1,
+	Unknown,
+}
 
 impl MMU {
 	pub fn new(romname: &str) -> MMU {
@@ -32,9 +39,21 @@ impl MMU {
 			intf: 0,
 			serial: Serial::new(),
 			timer: Timer::new(),
+			mbc: Unknown,
+			rombank: 1,
+			rambank: 0,
+			mbcmode: false,
+			ramon: false,
 		};
 
 		res.rom = File::open(&Path::new(romname)).read_to_end();
+		res.setmbc();
+		match res.mbc {
+			Unknown => { fail!("Unsupported MBC detected"); },
+			_ => {},
+		};
+
+		res.setram();
 
 		res.wb(0xFF05, 0);
 		res.wb(0xFF06, 0);
@@ -52,6 +71,30 @@ impl MMU {
 		return res;
 	}
 
+	fn setmbc(&mut self) {
+		self.mbc =
+		if self.rom.len() < 0x147 { Unknown }
+		else {
+			match self.rom[0x147] {
+				0x00 => Direct,
+				0x01 => MBC1,
+				_ => Unknown,
+			}
+		};
+	}
+
+	fn setram(&mut self) {
+		if self.rom.len() < 0x149 { fail!("Rom size to small"); };
+		let ramsize = match self.rom[0x149] {
+			0 => 0,
+			1 => 0x800,
+			2 => 0x2000,
+			3 => 0x8000,
+			_ => 0,
+		};
+		self.ram.grow(ramsize, &0);
+	}
+
 	pub fn cycle(&mut self, ticks: uint) {
 		self.timer.step(ticks);
 		self.intf |= self.timer.interrupt;
@@ -60,9 +103,10 @@ impl MMU {
 
 	pub fn rb(&self, address: u16) -> u8 {
 		match address {
-			0x0000 .. 0x7FFF => self.readrom(address),
+			0x0000 .. 0x3FFF => self.rom[address],
+			0x4000 .. 0x7FFF => self.readrombank(address),
 			//0x8000 .. 0x9FFF => { 0 }, // VRAM
-			//0xA000 .. 0xBFFF => { 0 }, // ext ram
+			0xA000 .. 0xBFFF => self.readram(address),
 			0xC000 .. 0xFDFF => self.wram[address & 0x1FFF],
 			//0xFF00 => { 0 }, // Keypad
 			0xFF01 .. 0xFF02 => self.serial.rb(address),
@@ -80,16 +124,36 @@ impl MMU {
 		(self.rb(address) as u16) | (self.rb(address + 1) as u16 << 8)
 	}
 
-	fn readrom(&self, address: u16) -> u8 {
-		// TODO: MBC
-		return self.rom[address & 0x7FFF];
+	fn readrombank(&self, address: u16) -> u8 {
+		match self.mbc {
+			Unknown => { 0 },
+			Direct => self.rom[address],
+			MBC1 =>   self.rom[(self.rombank * 0x4000) | (address & 0x3FFF)],
+		}
+	}
+
+	fn ramaddress(&self, address: u16) -> Option<u16> {
+		match self.mbc {
+			Unknown => None,
+			Direct => Some(address),
+			MBC1 => if !self.ramon || !self.mbcmode { None } else {
+				Some(self.rambank * 0x2000 | address)
+			},
+		}
+	}
+
+	fn readram(&self, address: u16) -> u8 {
+		match self.ramaddress(address) {
+			None => 0,
+			Some(n) => self.ram[n],
+		}
 	}
 
 	pub fn wb(&mut self, address: u16, value: u8) {
 		match address {
 			0x0000 .. 0x7FFF => self.writerom(address, value),
 			0x8000 .. 0x9FFF => {}, // VRAM
-			0xA000 .. 0xBFFF => {}, // ext ram
+			0xA000 .. 0xBFFF => self.writeram(address, value),
 			0xC000 .. 0xFDFF => self.wram[address & 0x1FFF] = value,
 			//0xFF00 => {}, // Keypad
 			0xFF01 .. 0xFF02 => { self.serial.wb(address, value); }, // Serial console
@@ -110,8 +174,32 @@ impl MMU {
 		self.wb(address + 1, (value >> 8) as u8);
 	}
 
-	fn writerom(&mut self, _address: u16, _value: u8) {
-		// TODO
+	fn writerom(&mut self, address: u16, value: u8) {
+		match self.mbc {
+			Unknown => {},
+			Direct => {},
+			MBC1 => {
+				match address {
+					0x0000 .. 0x1FFF => { self.ramon = (value == 0xA); },
+					0x2000 .. 0x3FFF => {
+						self.rombank = (self.rombank & 0x60) | (match value as u16 & 0x1F { 0 => 1, n => n });
+					},
+					0x4000 .. 0x5FFF => {
+						if !self.mbcmode { (self.rombank & 0x1F) | (((value as u16) & 0x03) << 5); }
+						else { self.rambank = value as u16 & 0x03; }
+					},
+					0x6000 .. 0x7FFF => { self.mbcmode = ((value & 1) == 1); }
+					_ => { fail!(""); },
+				}
+			}
+		}
+	}
+
+	fn writeram(&mut self, address: u16, value: u8) {
+		match self.ramaddress(address) {
+			None => {},
+			Some(n) => self.ram[n] = value,
+		};
 	}
 }
 
