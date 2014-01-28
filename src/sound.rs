9ef0f120ff50aa19c11f2af969144adf8dfa7944
Mathijs van de Nes <mvdnes@mvdnes.nl>
2014-01-28 16:55:34 +0100

    Fixed bug in Sound

--- src/sound.rs
+++ src/sound.rs
@@ -1,11 +1,11 @@
 
 pub struct Sound {
-	data: [u8,.. 0x16],
+	data: [u8,.. 0x17],
 }
 
 impl Sound {
 	pub fn new() -> Sound {
-		Sound { data: [0,.. 0x16] }
+		Sound { data: [0,.. 0x17] }
 	}
 
 	pub fn rb(&self, a: u16) -> u8 {
