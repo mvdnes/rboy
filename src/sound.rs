6df2c9719afa34b5615d0dd891742220219e6db4
Mathijs van de Nes <git@mathijs.vd-nes.nl>
2015-10-13 22:42:00 +0200

    We have some sound!

--- src/sound.rs
+++ src/sound.rs
@@ -16,16 +16,15 @@ pub struct Sound {
     channel3_on: bool,
     channel3_len: u8,
     channel3_vol: u8,
-    channel3_freq: u16,
-    channel3_counter: Counter,
+    channel3_freq: u32,
+    channel3_freq_div: u32,
+    channel3_uselen: bool,
+    channel3_started: bool,
+    channel3_wave_idx: usize,
     voice: Option<cpal::Voice>,
     blip: Option<BlipBuf>,
-}
-
-enum Counter {
-    Stopped,
-    Continuous,
-    Timed(u8),
+    bliptime: u32,
+    blipval: i32,
 }
 
 impl Sound {
@@ -48,9 +47,14 @@ impl Sound {
             channel3_len: 0,
             channel3_vol: 0,
             channel3_freq: 0,
-            channel3_counter: Counter::Stopped,
+            channel3_uselen: false,
+            channel3_started: false,
+            channel3_freq_div: 0,
+            channel3_wave_idx: 0,
             voice: voice,
             blip: blipbuf,
+            bliptime: 0,
+            blipval: 0,
         }
     }
 
@@ -71,70 +75,111 @@ impl Sound {
             0xFF1A => self.channel3_on = v & 0x80 == 0x80,
             0xFF1B => self.channel3_len = v,
             0xFF1C => self.channel3_vol = (v >> 5) & 0x3,
-            0xFF1D => self.channel3_freq = self.channel3_freq & 0xFF00 | v as u16,
+            0xFF1D => self.channel3_freq = self.channel3_freq & 0xFF00 | v as u32,
             0xFF1E => {
-                self.channel3_freq = self.channel3_freq & 0x00FF | (((v & 0x7) as u16) << 8);
-                match v & 0b_1100_0000 {
-                    0b_1000_0000 => self.channel3_counter = Counter::Continuous,
-                    0b_1100_0000 => self.channel3_counter = Counter::Timed(self.channel3_len),
-                    _ => (),
-                }
+                self.channel3_freq = self.channel3_freq & 0x00FF | (((v & 0x7) as u32) << 8);
+                self.channel3_started = v & 0x80 == 0x80;
+                self.channel3_uselen = v & 0x40 == 0x40;
+                self.channel3_wave_idx = 0;
             }
             0xFF30 ... 0xFF3F => self.waveram[a as usize - 0xFF30] = v,
             _ => (),
         }
     }
 
-    pub fn do_cycle(&mut self, _cycles: u32)
+    fn blip(&mut self) -> &mut BlipBuf {
+        self.blip.as_mut().unwrap()
+    }
+
+    fn channel(&mut self) -> &mut cpal::Voice {
+        self.voice.as_mut().unwrap()
+    }
+
+    pub fn do_cycle(&mut self, cycles: u32)
     {
-        // To be implemented
+        self.bliptime += cycles;
+        if self.channel3_started && self.channel3_on {
+            let rfreq = 64 * (2048 - (self.channel3_freq as u32));
+            self.channel3_freq_div += cycles;
+            while self.channel3_freq_div > rfreq {
+                self.channel3_freq_div -= rfreq;
+                self.channel3_wave_idx = (self.channel3_wave_idx + 1) % 32;
+                if self.channel3_uselen {
+                    self.channel3_len = self.channel3_len.wrapping_sub(1);
+                    if self.channel3_len == 0 {
+                        self.channel3_started = false;
+                    }
+                }
+                let sample = ((self.waveram[self.channel3_wave_idx / 2]
+                    >> (if self.channel3_wave_idx % 1 == 0 { 4 } else { 0 }))
+                    & 0x0F) as i32;
+                let newblip = sample * 1000 - self.blipval;
+                let time = self.bliptime;
+                if self.blip.is_some() {
+                    self.blip().add_delta(time, newblip);
+                    self.blipval = sample;
+                }
+            }
+        }
+        else if self.blipval != 0 && self.blip.is_some() {
+            let time = self.bliptime;
+            let newblip = -self.blipval;
+            self.blip().add_delta(time, newblip);
+            self.blipval = 0;
+        }
+        if self.bliptime > (1 << 16) && self.blip.is_some() {
+            self.blip().end_frame(1 << 16);
+            self.bliptime -= 1 << 16;
+            self.play_blipbuf();
+        }
+    }
+
+    fn play_blipbuf(&mut self) {
+        let channels_len = self.channel().format().channels.len();
+
+        while self.blip().samples_avail() > 0 {
+            let buf = &mut [0; 1024];
+            let count = self.blip().read_samples(buf, false);
+            let blipbuf = &buf[0..count];
+            let mut done = 0;
+            let mut lastdone = count;
+
+            while lastdone != done && done < count {
+                lastdone = done;
+                let channelbuf = &blipbuf[done..];
+                match self.channel().append_data(channelbuf.len()) {
+                    cpal::UnknownTypeBuffer::U16(mut buffer) => {
+                        for (sample, value) in buffer.chunks_mut(channels_len).zip(channelbuf) {
+                            let value = *value as u16 + std::i16::MAX as u16;
+                            for out in sample.iter_mut() { *out = value; }
+                            done += 1;
+                        }
+                    }
+                    cpal::UnknownTypeBuffer::I16(mut buffer) => {
+                        for (sample, value) in buffer.chunks_mut(channels_len).zip(channelbuf) {
+                            for out in sample.iter_mut() { *out = *value; }
+                            done += 1;
+                        }
+                    }
+                    cpal::UnknownTypeBuffer::F32(mut buffer) => {
+                        for (sample, value) in buffer.chunks_mut(channels_len).zip(channelbuf) {
+                            let value = *value as f32 / std::i16::MAX as f32;
+                            for out in sample.iter_mut() { *out = value; }
+                            done += 1;
+                        }
+                    }
+                }
+                self.channel().play();
+            }
+        }
     }
 }
 
 fn get_channel() -> Option<cpal::Voice> {
     if cpal::get_endpoints_list().count() == 0 { return None; }
+
     let endpoint = try_opt!(cpal::get_default_endpoint());
     let format = try_opt!(endpoint.get_supported_formats_list().ok().and_then(|mut v| v.next()));
     let channel = try_opt!(cpal::Voice::new(&endpoint, &format).ok());
     Some(channel)
-}
-
-fn play_blipbuf(channel: &mut cpal::Voice, blip: &mut BlipBuf) {
-    let channels_len = channel.format().channels.len();
-
-    while blip.samples_avail() > 0 {
-        let buf = &mut [0; 1024];
-        let count = blip.read_samples(buf, false);
-        let blipbuf = &buf[0..count];
-        let mut done = 0;
-        let mut lastdone = count;
-
-        while lastdone != done && done < count {
-            lastdone = done;
-            let channelbuf = &blipbuf[done..];
-            match channel.append_data(channelbuf.len()) {
-                cpal::UnknownTypeBuffer::U16(mut buffer) => {
-                    for (sample, value) in buffer.chunks_mut(channels_len).zip(channelbuf) {
-                        let value = *value as u16 + std::i16::MAX as u16;
-                        for out in sample.iter_mut() { *out = value; }
-                        done += 1;
-                    }
-                }
-                cpal::UnknownTypeBuffer::I16(mut buffer) => {
-                    for (sample, value) in buffer.chunks_mut(channels_len).zip(channelbuf) {
-                        for out in sample.iter_mut() { *out = *value; }
-                        done += 1;
-                    }
-                }
-                cpal::UnknownTypeBuffer::F32(mut buffer) => {
-                    for (sample, value) in buffer.chunks_mut(channels_len).zip(channelbuf) {
-                        let value = *value as f32 / std::i16::MAX as f32;
-                        for out in sample.iter_mut() { *out = value; }
-                        done += 1;
-                    }
-                }
-            }
-            channel.play();
-        }
-    }
 }
