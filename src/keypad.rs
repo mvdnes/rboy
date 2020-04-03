6432d6484b6044b67ac295f8007684af36e0595c
Mathijs van de Nes <git@mathijs.vd-nes.nl>
2020-04-03 17:03:55 +0200

    Correct the behaviour of the keypad
    - Logic of selection is inverted (0 = select)
    - Bit 6 and 7 are always '1'
    - Selecting multiple rows is now done by 'and' instead of 'or'
    - Interrupt on falling edge of selected keys
    
    Resolves #7

--- src/keypad.rs
+++ src/keypad.rs
@@ -22,53 +22,65 @@ impl Keypad {
         let mut res = Keypad {
             row0: 0x0F,
             row1: 0x0F,
-            data: 0,
+            data: 0xFF,
             interrupt: 0,
         };
         res.update();
         return res
     }
 
-    pub fn rb(&self) -> u8 {
+    pub fn rb(&mut self) -> u8 {
         self.data
     }
 
     pub fn wb(&mut self, value: u8) {
-        self.data = value;
+        self.data = 0xC0 | (value & 0x30) | (self.data & 0x0F);
         self.update();
     }
-    
+
     fn update(&mut self) {
-        self.data &= 0x30;
-        if self.data & 0x10 == 0x10 { self.data |= self.row0; }
-        if self.data & 0x20 == 0x20 { self.data |= self.row1; }
+        let old_values = self.data & 0xF;
+        let mut new_values = 0xF;
+
+        if self.data & 0x10 == 0x00 {
+            new_values &= self.row0;
+        }
+        if self.data & 0x20 == 0x00 {
+            new_values &= self.row1;
+        }
+
+        if old_values == 0xF && new_values != 0xF {
+            self.interrupt |= 0x10;
+        }
+
+        self.data = (self.data & 0xF0) | new_values;
     }
 
     pub fn keydown(&mut self, key: KeypadKey) {
         match key {
-            KeypadKey::Right => self.row1 &= !(1 << 0),
-            KeypadKey::Left => self.row1 &= !(1 << 1),
-            KeypadKey::Up => self.row1 &= !(1 << 2),
-            KeypadKey::Down => self.row1 &= !(1 << 3),
-            KeypadKey::A => self.row0 &= !(1 << 0),
-            KeypadKey::B => self.row0 &= !(1 << 1),
-            KeypadKey::Select => self.row0 &= !(1 << 2),
-            KeypadKey::Start => self.row0 &= !(1 << 3),
+            KeypadKey::Right =>  self.row0 &= !(1 << 0),
+            KeypadKey::Left =>   self.row0 &= !(1 << 1),
+            KeypadKey::Up =>     self.row0 &= !(1 << 2),
+            KeypadKey::Down =>   self.row0 &= !(1 << 3),
+            KeypadKey::A =>      self.row1 &= !(1 << 0),
+            KeypadKey::B =>      self.row1 &= !(1 << 1),
+            KeypadKey::Select => self.row1 &= !(1 << 2),
+            KeypadKey::Start =>  self.row1 &= !(1 << 3),
         }
-        self.interrupt |= 0x10;
+
         self.update();
     }
 
     pub fn keyup(&mut self, key: KeypadKey) {
         match key {
-            KeypadKey::Right => self.row1 |= 1 << 0,
-            KeypadKey::Left => self.row1 |= 1 << 1,
-            KeypadKey::Up => self.row1 |= 1 << 2,
-            KeypadKey::Down => self.row1 |= 1 << 3,
-            KeypadKey::A => self.row0 |= 1 << 0,
-            KeypadKey::B => self.row0 |= 1 << 1,
-            KeypadKey::Select => self.row0 |= 1 << 2,
-            KeypadKey::Start => self.row0 |= 1 << 3,
+            KeypadKey::Right =>  self.row0 |= 1 << 0,
+            KeypadKey::Left =>   self.row0 |= 1 << 1,
+            KeypadKey::Up =>     self.row0 |= 1 << 2,
+            KeypadKey::Down =>   self.row0 |= 1 << 3,
+            KeypadKey::A =>      self.row1 |= 1 << 0,
+            KeypadKey::B =>      self.row1 |= 1 << 1,
+            KeypadKey::Select => self.row1 |= 1 << 2,
+            KeypadKey::Start =>  self.row1 |= 1 << 3,
         }
         self.update();
     }
@@ -79,7 +91,7 @@ mod test {
     use super::KeypadKey;
 
     #[test]
-    fn keys_row0() {
+    fn keys_buttons() {
         let mut keypad = super::Keypad::new();
         let keys0 : [KeypadKey; 4] = [KeypadKey::A, KeypadKey::B, KeypadKey::Select, KeypadKey::Start];
 
@@ -87,23 +99,23 @@ mod test {
             keypad.keydown(keys0[i]);
 
             keypad.wb(0x00);
-            assert_eq!(keypad.rb(), 0x00);
+            assert_eq!(keypad.rb(), 0xCF & !(1 << i));
 
             keypad.wb(0x10);
-            assert_eq!(keypad.rb(), 0x1F & !(1 << i));
+            assert_eq!(keypad.rb(), 0xDF & !(1 << i));
 
             keypad.wb(0x20);
-            assert_eq!(keypad.rb(), 0x2F);
+            assert_eq!(keypad.rb(), 0xEF);
 
             keypad.wb(0x30);
-            assert_eq!(keypad.rb(), 0x3F);
+            assert_eq!(keypad.rb(), 0xFF);
 
             keypad.keyup(keys0[i]);
         }
     }
 
     #[test]
-    fn keys_row1() {
+    fn keys_direction() {
         let mut keypad = super::Keypad::new();
         let keys1 : [KeypadKey; 4] = [KeypadKey::Right, KeypadKey::Left, KeypadKey::Up, KeypadKey::Down];
 
@@ -111,16 +123,16 @@ mod test {
             keypad.keydown(keys1[i]);
 
             keypad.wb(0x00);
-            assert_eq!(keypad.rb(), 0x00);
+            assert_eq!(keypad.rb(), 0xCF & !(1 << i));
 
             keypad.wb(0x10);
-            assert_eq!(keypad.rb(), 0x1F);
+            assert_eq!(keypad.rb(), 0xDF);
 
             keypad.wb(0x20);
-            assert_eq!(keypad.rb(), 0x2F & !(1 << i));
+            assert_eq!(keypad.rb(), 0xEF & !(1 << i));
 
             keypad.wb(0x30);
-            assert_eq!(keypad.rb(), 0x3F);
+            assert_eq!(keypad.rb(), 0xFF);
 
             keypad.keyup(keys1[i]);
         }
